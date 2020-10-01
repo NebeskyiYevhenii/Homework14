2dfa4ff255a077f129b0adb4889b57ec17765ce9
NebeskyiYevhenii <evheniy.slav@gmail.com>
2020-10-01 16:34:45 +0300

    end

---
+++ Homework14/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> queue = new Queue<string>();
+            // добавление элементов
+            queue.AddToEnd("Саша");
+            queue.AddToEnd("Маша");
+            queue.AddToEnd("Даша");
+            queue.AddToEnd("Глаша");
+
+            // выводим элементы
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            // удаляем элемент
+            queue.Delete("Маша");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Delete \"Маша\"");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            // проверяем наличие элемента
+            bool isPresent = queue.Contains("Дима");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("проверяем наличие элемента \"Дима\"");
+            Console.WriteLine(isPresent == true ? "Дима присутствует" : "Sam отсутствует");
+
+            // проверяем наличие элемента
+            isPresent = queue.Contains("Глаша");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("проверяем наличие элемента \"Глаша\"");
+            Console.WriteLine(isPresent == true ? "Глаша присутствует" : "Sam отсутствует");
+
+            // добавляем элемент в начало            
+            queue.AddToStart("Сергей");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("добавляем элемент в начало  \"Сергей\"");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            // добавляем элемент в конец            
+            queue.AddToEnd("Яна");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("добавляем элемент в конец  \"Яна\"");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            // добавляем элемент в индекс 3            
+            queue.AddByIndex("Миша", 3);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("добавляем элемент в индекс 3  \"Миша\"");
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            // получаем 3 элемент
+            //isPresent = linkedList.Contains("Bill!!!!");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("получаем 3 элемент");
+            Console.WriteLine(queue[3].Data);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework14/Queue.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework14
+{
+    public class Human<T>
+    {
+        public T Data { get; set; }
+        public Human<T> Next { get; set; }
+        public Human(T data)
+        {
+            Data = data;
+        }
+    }
+    public class Queue<T> : IEnumerable<T>
+    {
+        Human<T> head;
+        Human<T> tail;
+        int count;
+
+        public void AddByIndex(T data, int index)
+        {
+            Human<T> newNode = new Human<T>(data);
+            Human<T> node = head;
+            Human<T> previous = null;
+            int i = 1;
+
+            while (i <= count)
+            {
+                if (i == index)
+                {
+                    previous.Next = newNode;
+                    newNode.Next = node;
+                    break;
+                }
+                previous = node;
+                node = node.Next;
+                i++;
+            }
+            count++;
+        }
+
+        public void AddToEnd(T data)
+        {
+            Human<T> node = new Human<T>(data);
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+                tail.Next = node;
+            tail = node;
+
+            count++;
+        }
+
+        public void AddToStart(T data)
+        {
+            Human<T> node = new Human<T>(data);
+
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                node.Next = head;
+                head = node;
+                count++;
+            }
+        }
+
+        public bool Delete(T data)
+        {
+            Human<T> item = head;
+            Human<T> previous = null;
+            bool Return = false;
+            if (head != null)
+            {
+                while (item != null)
+                {
+                    if (item.Data.Equals(data))
+                    {
+                        previous.Next = item.Next;
+                        count--;
+                        Return = true;
+                    }
+                    previous = item;
+                    item = item.Next;
+                }
+                return Return;
+            }
+            return false;
+        }
+
+        public Human<T> this[int index]
+        {
+            set
+            {
+                Human<T> node = head;
+                int i = 1;
+
+                while (i <= index)
+                {
+                    if (i == index)
+                    {
+                        node = value;
+                    }
+                    node = node.Next;
+                    i++;
+                }
+            }
+            get
+            {
+
+                Human<T> indexNode = head;
+                Human<T> node = head;
+                Human<T> previous = null;
+                int i = 1;
+
+                while (i <= index)
+                {
+                    if (i == index)
+                    {
+                        return node;
+                    }
+                    previous = node;
+                    node = node.Next;
+                    i++;
+                }
+
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Human<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+    }
+
+}
